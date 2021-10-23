52a2119cbac6b9de39878a7b3a74fb8d589b8d93
KINGvcBOT <84507735+Imteyazking01@users.noreply.github.com>
2021-10-23 16:11:27 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -18,7 +18,7 @@ async def start(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🔨Source Code🔨", url="https://github.com/xabhish3k/camillavcbot2.0")
+                        "🔨Source Code🔨", url="https://github.com/Imteyazking01/music-listen-bot")
                   ],[
                     InlineKeyboardButton(
                         "sᴜᴘᴘᴏʀᴛ👿", url="https://t.me/warbotzsupport"
